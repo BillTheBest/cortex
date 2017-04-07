d488c89075c49fc67b6d3fa36d50f0fd0c4479e5
Aaron Kirkbride <aaron@aaronkirkbride.com>
2017-04-07 15:33:08 +0100

    Update bucket comment

--- chunk/schema.go
+++ chunk/schema.go
@@ -623,7 +623,7 @@ func (cfg *SchemaConfig) tableForBucket(bucketStart int64) string {
 	return cfg.TablePrefix + strconv.Itoa(int(bucketStart/int64(cfg.TablePeriod/time.Second)))
 }
 
-// Bucket is a range of time with a tableName and a hashKey
+// Bucket desribes a range of time with a tableName and hashKey
 type Bucket struct {
 	from      uint32
 	through   uint32
